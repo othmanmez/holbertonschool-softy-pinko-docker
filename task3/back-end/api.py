1eea2faadbe35c3d05e564613589c30a0c88a70b
Pulse Car Location <contact@pulsecarlocation.fr>
2025-05-06 11:51:36 +0200

    docker

--- task3/back-end/api.py
+++ task3/back-end/api.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Permet les requêtes CORS (Cross-Origin) pour autoriser le front-end à accéder à l'API
+CORS(app)
 
 @app.route('/api/hello')
 def hello_world():
-    return 'Hello, World!'  # Ce message sera renvoyé au front-end
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5252)  # L'API écoute sur le port 5252
+    app.run(host='0.0.0.0', port=5252)
